be469b72774c2da7e0152f09363cbd88877a0d0c
Robert Parker <robbybparker@gmail.com>
2026-01-19 22:55:17 -0500

    start constructing inputs to send to canos

--- moai-canos.jl
+++ moai-canos.jl
@@ -1,30 +1,84 @@
 ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
-import JuMP
-import Ipopt
-import MathOptInterface as MOI
-import PythonCall
+using JuMP
+using Ipopt
+using LinearAlgebra
+using MathOptInterface
+using PythonCall
 import MathOptAI as MOAI
+import PowerModels
+import PGLib
+import MathProgIncidence
 
-# Requires torch and torch_geometric
+# Load the CANOS predictor and its saved template/shapes.
 PythonCall.pyimport("sys").path.append(pwd())
 PythonCall.pyimport("vectorcanos")
-predictor = MOAI.PytorchModel("vector-canos.pt")
+torch = PythonCall.pyimport("torch")
 
-N = 312
-ipopt = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "tol" => 1e-6) #, "linear_solver" => "ma57")
-model = JuMP.Model(ipopt)
+#modelpath = joinpath("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_121912", "model.pt")
+modelpath = "vectorcanos-trained.pt"
+nn = torch.load(modelpath, map_location = "cpu")
+predictor = MOAI.PytorchModel(modelpath)
 
-import PowerModels as PM
-casefile = joinpath("data_generation", "pglib", "pglib_opf_case14_ieee.m")
-casedata = PM.parse_file(casefile)
-pm = PM.instantiate_model(casedata, PM.ACPPowerModel, PM.build_pf; jump_model = model)
+pglib_data = PGLib.pglib("case14")
+pm = PowerModels.instantiate_model(pglib_data, PowerModels.ACPPowerModel, PowerModels.build_pf)
 
-#JuMP.set_optimizer_attribute(model, "linear_solver", "ma57")
-JuMP.@variable(model, x[1:N], start = 0.5)
-y, formulation = MOAI.add_predictor(model, predictor, x, gray_box = true)
-xref = ones(N)
-#JuMP.@objective(model, Min, sum((x .- xref).^2))
-# I just chose random numbers here, but we'd want to constrain a voltage or something
-# to go over its limit.
-JuMP.@constraint(model, y[2] >= 1.0)
-JuMP.optimize!(model)
+PythonCall.pyimport("sys").path.append(pwd())
+pfdelta_variants = PythonCall.pyimport("core.datasets.pfdelta_variants")
+PFDeltaCANOS = pfdelta_variants.PFDeltaCANOS
+root_dir = joinpath("data", "pfdelta_data")
+dataset = PFDeltaCANOS(
+    add_bus_type = true,
+    case_name = "case14",
+    model = "CANOS",
+    root_dir = root_dir,
+    split = "train",
+    task = "1.1",
+)
+py_x0 = dataset[0]
+
+py_x0_flat = nn.flatten_input(py_x0)
+x0_flat = PythonCall.pyconvert(Vector{Float64}, py_x0_flat)
+# I need to extract loads out of this input data and set them in the PM data
+# before building the model
+
+# Map variables to _input_ data
+function get_inputs(pm::PowerModels.AbstractPowerModel)
+    ref = pm.ref[:it][:pm][:nw][0]
+    buskeys = sort(collect(keys(pm.data["bus"]); by = k -> parse(Int, k)))
+    # Inputs could be numbers, variables, or expressions
+    inputs = Any[]
+    # Bus inputs
+    # bus_type == 3 => reference bus
+    # bus_type == 2 => generator (PV) bus
+    # bus_type == 1 => load (PQ) bus
+    gen_buses = filter(i -> pm.data["bus"][i]["bus_type"] == 2, buskeys)
+    load_buses = filter(i -> pm.data["bus"][i]["bus_type"] == 1, buskeys)
+    slack_buses = filter(i -> pm.data["bus"][i]["bus_type"] == 3, buskeys)
+    for i in buskeys
+        idx = parse(Int, i)
+        bus_type = pm.data["bus"][i]["bus_type"]
+        if bus_type == 1
+            pd = sum(ref[:load][l]["pd"] for l in ref[:bus_loads][idx])
+            qd = sum(ref[:load][l]["qd"] for l in ref[:bus_loads][idx])
+            append!(inputs, [pd, qd])
+        elseif bus_type == 2
+            # Since the input calls for total injection/demand, I sum up generator
+            # active power variables at each node.
+            pg = sum(ref[:gen][g]["pg"] for g in ref[:bus_gens][idx])
+            vm = PowerModels.var(pm, :vm, idx)
+            append!(inputs, [pg, vm])
+        elseif bus_type == 3
+            va = PowerModels.var(pm, :va, idx)
+            vm = PowerModels.var(pm, :vm, idx)
+            append!(inputs, [va, vm])
+        else
+            error("Unexpected bus type $(bus_type)")
+        end
+    end
+    return inputs
+end
+
+# 1. Add any variables that are necessary
+# 2. Load point from dataset, map variables to values from this point, construct objective.
+# 3. Deactivate bounds on ACPF outputs.
+# 4. Add bound constraining _some_ output to be above its limit
