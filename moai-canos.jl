ab5ab9836c9baac9625acc9b84a8e43109df3306
Robert Parker <robbybparker@gmail.com>
2026-01-22 12:22:11 -0500

    quick optimizaiton problem

--- moai-canos.jl
+++ moai-canos.jl
@@ -4,25 +4,24 @@ using Ipopt
 using LinearAlgebra
 using MathOptInterface
 using PythonCall
+using Printf
 import MathOptAI as MOAI
 import PowerModels
 import PGLib
 import MathProgIncidence
 
-# Load the CANOS predictor and its saved template/shapes.
+# Python imports
 PythonCall.pyimport("sys").path.append(pwd())
 PythonCall.pyimport("vectorcanos")
 torch = PythonCall.pyimport("torch")
 
+# Load CANOS NN model
 #modelpath = joinpath("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_121912", "model.pt")
 modelpath = "vectorcanos-trained.pt"
 nn = torch.load(modelpath, map_location = "cpu")
 predictor = MOAI.PytorchModel(modelpath)
 
-pglib_data = PGLib.pglib("case14")
-pm = PowerModels.instantiate_model(pglib_data, PowerModels.ACPPowerModel, PowerModels.build_pf)
-
-PythonCall.pyimport("sys").path.append(pwd())
+# Load dataset that we will use for target data
 pfdelta_variants = PythonCall.pyimport("core.datasets.pfdelta_variants")
 PFDeltaCANOS = pfdelta_variants.PFDeltaCANOS
 root_dir = joinpath("data", "pfdelta_data")
@@ -34,12 +33,6 @@ dataset = PFDeltaCANOS(
     split = "train",
     task = "1.1",
 )
-py_x0 = dataset[0]
-
-py_x0_flat = nn.flatten_input(py_x0)
-x0_flat = PythonCall.pyconvert(Vector{Float64}, py_x0_flat)
-# I need to extract loads out of this input data and set them in the PM data
-# before building the model
 
 # Map variables to _input_ data
 function get_inputs(pm::PowerModels.AbstractPowerModel)
@@ -81,19 +74,26 @@ function get_inputs(pm::PowerModels.AbstractPowerModel)
         end
     end
     for i in branchkeys
-        r = data["branch"][i]["br_r"]
-        x = data["branch"][i]["br_x"]
-        gf = data["branch"][i]["g_fr"]
-        bf = data["branch"][i]["b_fr"]
-        gt = data["branch"][i]["g_to"]
-        bt = data["branch"][i]["b_to"]
-        tap = data["branch"][i]["tap"]
-        shift = data["branch"][i]["shift"]
+        r = pm.data["branch"][i]["br_r"]
+        x = pm.data["branch"][i]["br_x"]
+        gf = pm.data["branch"][i]["g_fr"]
+        bf = pm.data["branch"][i]["b_fr"]
+        gt = pm.data["branch"][i]["g_to"]
+        bt = pm.data["branch"][i]["b_to"]
+        tap = pm.data["branch"][i]["tap"]
+        shift = pm.data["branch"][i]["shift"]
         append!(branch_inputs, [r, x, gf, bf, gt, bt, tap, shift])
     end
     inputs = vcat(bus_inputs, pq_inputs, pv_inputs, slack_inputs, branch_inputs)
     return inputs
 end
+
+py_x0 = dataset[0]
+py_x0_flat = nn.flatten_input(py_x0)
+x0_flat = PythonCall.pyconvert(Vector{Float64}, py_x0_flat)
+pglib_data = PGLib.pglib("case14")
+load_pfd_into_pm!(pglib_data, py_x0)
+pm = PowerModels.instantiate_model(pglib_data, PowerModels.ACPPowerModel, PowerModels.build_pf)
 
 # 1. Add any variables that are necessary
 # 2. Load point from dataset, map variables to values from this point, construct objective.
@@ -101,3 +101,24 @@ end
 # 4. Add bound constraining _some_ output to be above its limit
 
 inputs = get_inputs(pm)
+n_inputs = length(inputs)
+
+# Minimize 1-norm of difference between inputs and our target inputs.
+JuMP.@variable(pm.model, input_slack_pos[1:n_inputs] >= 0.0, start = 0.0)
+JuMP.@variable(pm.model, input_slack_neg[1:n_inputs] >= 0.0, start = 0.0)
+JuMP.@constraint(pm.model, input_slack_eqn,
+    inputs .- x0_flat .+ input_slack_pos .- input_slack_neg .== 0.0
+)
+JuMP.@objective(pm.model, Min, sum(input_slack_pos .+ input_slack_neg))
+
+JuMP.set_optimizer(pm.model, Ipopt.Optimizer)
+JuMP.optimize!(pm.model)
+
+# I don't expect to get zero error here unless I've updated parameters in PM.data
+#for i in 1:n_inputs
+#    lb = JuMP.has_lower_bound
+#    println(
+#        @sprintf("%10.2f", inputs[i])
+#        * @sprintf("%10.2f", x0_flat[i])
+#    )
+#end
