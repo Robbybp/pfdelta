9e55f3074deb11fcf6f0e5a99cb470b5bfa405f0
Robert Parker <rbparker@lanl.gov>
2026-05-25 18:48:34 -0600

    root dir

--- download.py
+++ download.py
@@ -1,7 +1,8 @@
+import os
 from core.datasets.pfdelta_dataset import PFDeltaDataset
 
 dataset = PFDeltaDataset(
     case_name="case14",
     task=1.1,
-    root_dir="data",
+    root_dir=os.path.join("data", "pfdelta_data"),
 )
