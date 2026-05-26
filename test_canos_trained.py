5c82521db91ea44e70461cf4d27ea7115b8a87c9
Robert Parker <rbparker@lanl.gov>
2026-05-25 20:40:31 -0600

    evaluate trained canos model on device

--- test_canos_trained.py
+++ test_canos_trained.py
@@ -23,12 +23,16 @@ canos = CANOS_PF(dataset, hidden_dim, include_sent_messages, k_steps)
 nparam = sum(p.numel() for p in canos.parameters() if p.requires_grad)
 print(f"N. parameters: {nparam}")
 
-modelpath = os.path.join("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_121912", "model.pt")
+modelpath = os.path.join("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_133938", "model.pt")
 canos_state = torch.load(modelpath, map_location="cpu")
 canos.load_state_dict(canos_state)
 canos.eval()
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 x0 = dataset[0]
+x0.to(device)
+canos.to(device)
 for node_type in x0.num_node_features.keys():
     print(f"{node_type}: {x0.num_node_features[node_type]}")
     print(x0[node_type])
