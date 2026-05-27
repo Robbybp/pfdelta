5a4015c39318a1abc157fac6f0871d3e3a1de428
Robert Parker <rbparker@lanl.gov>
2026-05-27 16:00:56 -0600

    use saved model weights

--- validate_canos.py
+++ validate_canos.py
@@ -50,7 +50,10 @@ canos = CANOS_PF(dataset, hidden_dim, include_sent_messages, k_steps)
 nparam = sum(p.numel() for p in canos.parameters() if p.requires_grad)
 print(f"N. parameters: {nparam}")
 
-modelpath = os.path.join("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_121912", "model.pt")
+# This is the old model (on Selene, I believe)
+#modelpath = os.path.join("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260116_133938", "model.pt")
+# This is the model after the PFDelta bugfix, on Selene
+modelpath = os.path.join("runs", "canos_task_1_1", "canos_k_steps15_hd128_lr5e-4_task_1_1_260527_121647", "model.pt")
 canos_state = torch.load(modelpath, map_location="cpu")
 canos.load_state_dict(canos_state)
 canos.eval()
